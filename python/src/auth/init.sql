bb43b70f14913da456a21902d848e0367ff44064
manurajeev <manurajeev.720@gmail.com>
2023-08-20 23:45:11 -0500

    auth setup

---
+++ python/src/auth/init.sql
@@ -0,0 +1,17 @@
+CREATE USER IF NOT EXISTS 'auth_user'@'localhost' IDENTIFIED BY 'auth_pass';
+
+DROP DATABASE IF EXISTS auth;
+CREATE DATABASE auth;
+
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO user (email, password) VALUES ('manu@email.com', 'Admin123');
+
